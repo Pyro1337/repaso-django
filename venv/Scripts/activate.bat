837e0f88a0c2647e05acff71ffdb2e7abf354c50
Ivan Sanchez <sanchezrivan2001@gmail.com>
2025-03-17 21:58:16 -0300

    subo todo de nuevo pq se fue alv

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\MSI\Desktop\Proyectos GITHUB\repaso-django\venv
+set VIRTUAL_ENV=C:\Users\user\Desktop\Proyectos GITHUB\repaso-django\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
