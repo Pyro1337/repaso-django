96b83387edd86557a39aa61842c82f74f6477b5f
Ivan Sanchez <sanchezrivan2001@gmail.com>
2025-03-20 17:15:00 -0300

    Se agrega login, signin , signout

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path #importamos path de django
-from myapp.views  import signup,landing_page,about,specific_salute,specific_project,projects,specific_task,tasks,create_task,create_project 
-
+from myapp.views  import signup,landing_page,about,specific_project,projects,specific_task,tasks,create_task,create_project,signout,signin
 
 #Creamos y colocamos las rutas aqui
 urlpatterns = [
-    path('',signup),
+    path('',signin),
     path('landing_page/',landing_page),
+    path('signup/',signup),
+    path('logout/',signout),
+    path('signin/',signin),
     path('about/',about),
-    path('<str:username>',specific_salute),
+   # path('<str:username>',specific_salute), ya no se usa
     path('projects/',projects),
     path('projects/<int:id>',specific_project),
     path('tasks/',tasks),
--- myapp/views.py
+++ myapp/views.py
@@ -1,68 +1,114 @@
 from django.shortcuts import render
-from django.http import HttpResponse,JsonResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404, render, redirect
-from .models import Project,Task
-from django.contrib.auth.forms import UserCreationForm 
-from django.contrib.auth.models import User #Importamos el modelo de usuario
-from .forms import CreateNewTask,CreateNewProject #importamos el formulario
+from .models import Project, Task
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User  # Importamos el modelo de usuario
+from .forms import CreateNewTask, CreateNewProject  # importamos el formulario
+from django.db import IntegrityError #Para manejar los errores de integridad con respecto al usuario
+from django.contrib.auth import login, logout, authenticate # para manejar el login de los usuarios con su respectiva clave o cookie
+from django.contrib.auth.forms import AuthenticationForm #importamos la autenticacion del usuario.
+
 
 # Create your views here.
+
+
 def signup(request):
     if request.method == 'GET':
-         return render(request,"signup.html",{
-        'form':UserCreationForm
+        return render(request, "signup.html", {
+            'form': UserCreationForm
         })
     else:
         if request.POST['password1'] == request.POST['password2']:
             try:
-                user = User.objects.create_user(username= request.POST['username'], password = request.POST['password1'])
+                user = User.objects.create_user(
+                    username=request.POST['username'], password=request.POST['password1'])
                 user.save()
-                return HttpResponse('Usuario creado con exito')
-            except:
-                return HttpResponse('El usuario ya existe')
-        return HttpResponse('Las contraseñas no coinciden')
-
-
-   
+                #una vez guardamos el usuario lo logeamos
+                login(request,user)
+                return redirect('/landing_page/') #redigirimos a la pagina principal
+            except IntegrityError:
+                return render(request, "signup.html", {
+                    'form': UserCreationForm,
+                    'error': "El usuario ya existe"
+                })
+                
+        return render(request, "signup.html", {
+            'form': UserCreationForm,
+            'error': "Las contraseñas no coinciden"
+        })
 
 
 def landing_page(request):
-    return render(request,"index.html")
+    return render(request, "index.html")
 
-def specific_salute(request, username):
-    return HttpResponse("<h2>Hello Sr. %s" %username)
+
+def signout(request):
+    logout(request)
+    response = redirect('/signin')
+    response.delete_cookie('sessionid')
+    return response
+
+def signin(request):
+    if request.method == 'GET':
+        return render(request, "signin.html", {
+            'form': AuthenticationForm
+        })
+    else:
+       user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+       if user is None:
+            return render(request, "signin.html",{
+           'form': AuthenticationForm,
+           'error': "El usuario o la contraseña son incorrectos"
+            })
+       else:
+           login(request, user)
+           return redirect('/landing_page/')
+
+
+
+
+#def specific_salute(request, username):
+   # return HttpResponse("<h2>Hello Sr. %s" % username)
+
 
 def about(request):
     autor_pagina = 'Ivan Sanchez'
-    return render(request,"about.html",{'autor_pagina':autor_pagina})
+    return render(request, "about.html", {'autor_pagina': autor_pagina})
 
-def specific_project(request,id):
-    projects = get_object_or_404(Project,id=id)
-    return HttpResponse("<h2>Nombre del proyecto: %s</h2>"%projects.name)
+
+def specific_project(request, id):
+    projects = get_object_or_404(Project, id=id)
+    return HttpResponse("<h2>Nombre del proyecto: %s</h2>" % projects.name)
+
 
 def projects(request):
-    #projects = list(Project.objects.values())
-    #return JsonResponse(projects, safe = False)
-    projects= Project.objects.all()
-    return render(request,'projects.html',{
-        'projects':projects
+    # projects = list(Project.objects.values())
+    # return JsonResponse(projects, safe = False)
+    projects = Project.objects.all()
+    return render(request, 'projects.html', {
+        'projects': projects
     })
 
-def specific_task(request,id):
-    tasks = get_object_or_404(Task, id = id)
-    return HttpResponse("<h3>Nombre de la tarea: %s </h3>"%tasks.title)
 
-def tasks (request):
-    #tasks = list(Task.objects.values())
-    #return JsonResponse(tasks, safe = False)
+def specific_task(request, id):
+    tasks = get_object_or_404(Task, id=id)
+    return HttpResponse("<h3>Nombre de la tarea: %s </h3>" % tasks.title)
+
+
+def tasks(request):
+    # tasks = list(Task.objects.values())
+    # return JsonResponse(tasks, safe = False)
     projects = Project.objects.all()
     tasks = Task.objects.all()
-    return render(request, 'tasks.html',{
+    return render(request, 'tasks.html', {
         'projects': projects,
         'tasks': tasks
     })
 
-#declaramos el formulario
+# declaramos el formulario
+
+
 def create_task(request):
     projects = Project.objects.all()  # ✅ Obtener todos los proyectos correctamente
 
@@ -74,11 +120,12 @@ def create_task(request):
     else:
         form = CreateNewTask(request.POST)
         if form.is_valid():
-            project = get_object_or_404(Project, id=request.POST['project'])  # ✅ Obtener el proyecto correcto
+            # ✅ Obtener el proyecto correcto
+            project = get_object_or_404(Project, id=request.POST['project'])
             Task.objects.create(
                 title=form.cleaned_data['title'],
                 description=form.cleaned_data['description'],
-                project=project  
+                project=project
             )
             return redirect('/tasks/')
         else:
@@ -87,22 +134,23 @@ def create_task(request):
                 'projects': projects,  # ✅ Asegurar que los proyectos se envían
                 'error': 'Formulario inválido'
             })
-        
+
+
 def create_project(request):
     if request.method == 'GET':
-        return render(request,'create_project.html',{
+        return render(request, 'create_project.html', {
             'form': CreateNewProject()
         })
     else:
         form = CreateNewProject(request.POST)
         if form.is_valid():
             Project.objects.create(
-                name = form.cleaned_data['name'],
-                description = form.cleaned_data['description']
+                name=form.cleaned_data['name'],
+                description=form.cleaned_data['description']
             )
             return redirect('/projects/')
         else:
-            return render(request,'create_project.html',{
+            return render(request, 'create_project.html', {
                 'form': form,
                 'error': 'Formulario inválido'
             })
